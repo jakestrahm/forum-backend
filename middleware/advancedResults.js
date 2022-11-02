fc9e618c5521b15f437b4accf2f87b5e7d431dd4
Jake Strahm <jakestrahm@Jakes-MacBook-Pro-2.local>
2022-11-02 12:42:58 -0400

    cleared out old documenation, preparing for docgen and last features

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -1,31 +1,31 @@
 const advancedResults = (model) => async (req, res, next) => {
     let query;
 
-    // Copy req.query
+    // copy req.query
     const reqQuery = { ...req.query };
 
-    // Fields to exclude
+    // fields to exclude
     const removeFields = ['select', 'sort', 'page', 'limit'];
 
-    // Loop over removeFields and delete them from reqQuery
+    // loop over removefields and delete them from reqquery
     removeFields.forEach(param => delete reqQuery[param]);
 
-    // Create query string
+    // create query string
     let queryStr = JSON.stringify(reqQuery);
 
-    // Create operators ($gt, $gte, etc)
+    // create operators ($gt, $gte, etc)
     queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
 
-    // Finding resource
+    // finding resource
     query = model.find(JSON.parse(queryStr));
 
-    // Select Fields
+    // select fields
     if (req.query.select) {
         const fields = req.query.select.split(',').join(' ');
         query = query.select(fields);
     }
 
-    // Sort
+    // sort
     if (req.query.sort) {
         const sortBy = req.query.sort.split(',').join(' ');
         query = query.sort(sortBy);
@@ -33,7 +33,7 @@ const advancedResults = (model) => async (req, res, next) => {
         query = query.sort('-createdAt');
     }
 
-    // Pagination
+    // pagination
     const page = parseInt(req.query.page, 10) || 1;
     const limit = parseInt(req.query.limit, 10) || 25;
     const startIndex = (page - 1) * limit;
@@ -42,10 +42,10 @@ const advancedResults = (model) => async (req, res, next) => {
 
     query = query.skip(startIndex).limit(limit);
 
-    // Executing query
+    // executing query
     const results = await query;
 
-    // Pagination result
+    // pagination result
     const pagination = {};
 
     if (endIndex < total) {
